c312ece0112e34d10bb9872dafe8087f97048075
PC-20190616HMDA\Administrator <1558628010@qq.com>
2019-09-26 16:35:11 +0800

    完善MD5工具类

--- library-management/src/main/java/com/book/management/util/Md5Util.java
+++ library-management/src/main/java/com/book/management/util/Md5Util.java
@@ -1,7 +1,16 @@
 package com.book.management.util;
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.crypto.hash.Md5Hash;
+
+import java.io.FileInputStream;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Arrays;
 
 
 /**
@@ -12,24 +21,222 @@ public class Md5Util {
     public static final String SALT = "Book";
 
     /**
-     * Md5加密
+     * (32位)Md5加密
      * @param str
      * @param salt
      * @return
      */
     public static String md5(String str, String salt) {
-        if (StringUtils.isEmpty(str)) {
+        if (StringUtils.isBlank(str)) {
             return null;
         } else {
+            if(StringUtils.isBlank(salt)){
+                //不加盐
+                return new Md5Hash(str).toString();
+            }
+            //盐值加密
             return new Md5Hash(str, salt).toString();
         }
     }
 
-    public static void main(String args[]) {
-        String password = "123456";
-//        String password = UUID.randomUUID().toString().replaceAll("-", "");
-//        System.out.println(password);
-        System.out.println("Md5加密后" + Md5Util.md5(password, Md5Util.SALT));
+    /**
+     * (16位)MD5编码
+     * @param str
+     * @param salt
+     * @return
+     */
+    public static String baseEcode16(String str,String salt){
+        String encodeStr = md5(str,salt);
+        if(StringUtils.isBlank(encodeStr)){
+            return null;
+        }else{
+            //截取32位码中间的16位
+            return encodeStr.substring(8,24);
+        }
+    }
+
+    /**
+     * MD5文件校验对比
+     * @param fileOne
+     * @param fileTwo
+     * @return
+     * @throws Exception
+     */
+    public static boolean md5CkeckFile(String fileOne,String fileTwo) throws Exception{
+        FileInputStream fis= new FileInputStream(fileOne);
+        String fileOneMd5 = DigestUtils.md5Hex(IOUtils.toByteArray(fis));
+        //关闭流
+        IOUtils.closeQuietly(fis);
+
+        FileInputStream fis2= new FileInputStream(fileTwo);
+        String fileTwoMd5 = DigestUtils.md5Hex(IOUtils.toByteArray(fis2));
+        //关闭流
+        IOUtils.closeQuietly(fis2);
+        if(fileOneMd5.equals(fileTwoMd5)){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Md5加密，返回32的字符串
+     * @param str
+     * @return
+     */
+    public static String encrypt32(String str) {
+        if (str == null) {
+            return null;
+        }
+        String result = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes("utf-8"));
+            byte         b[] = md.digest();
+            int          i;
+            StringBuffer buf = new StringBuffer("");
+            for (int offset = 0; offset < b.length; offset++) {
+                i = b[offset];
+                if (i < 0) {
+                    i += 256;
+                }
+                if (i < 16) {
+                    buf.append("0");
+                }
+                buf.append(Integer.toHexString(i));
+            }
+            result = buf.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * Md5加密，返回16的字符串(16位的密文就是32位中间的16位)
+     * @param str
+     * @return
+     */
+    public static String encrypt16(String str) {
+        if (str == null) {
+            return null;
+        }
+        return encrypt32(str).substring(8, 24);
+    }
+
+    private static final String HEX_NUMS_STR="0123456789ABCDEF";
+    private static final Integer SALT_LENGTH = 12;
+
+    /**
+     * 将16进制字符串转换成字节数组
+     * @param hex 输入字符串
+     * @return
+     */
+    public static byte[] hexStringToByte(String hex) {
+        int len = (hex.length() / 2);
+        byte[] result = new byte[len];
+        char[] hexChars = hex.toCharArray();
+        for (int i = 0; i < len; i++) {
+            int pos = i * 2;
+            result[i] = (byte) (HEX_NUMS_STR.indexOf(hexChars[pos]) << 4
+                    | HEX_NUMS_STR.indexOf(hexChars[pos + 1]));
+        }
+        return result;
+    }
+
+    /**
+     * 将指定byte数组转换成16进制字符串
+     * @param b
+     * @return
+     */
+    public static String byteToHexString(byte[] b) {
+        StringBuffer hexString = new StringBuffer();
+        for (int i = 0; i < b.length; i++) {
+            String hex = Integer.toHexString(b[i] & 0xFF);
+            if (hex.length() == 1) {
+                hex = '0' + hex;
+            }
+            hexString.append(hex.toUpperCase());
+        }
+        return hexString.toString();
+    }
+
+    /**
+     * 获得加密后的16进制形式口令
+     * @param password
+     * @return
+     * @throws NoSuchAlgorithmException
+     * @throws UnsupportedEncodingException
+     */
+    public static String getEncryptedPwd(String password)
+            throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        //声明加密后的口令数组变量
+        byte[] pwd = null;
+        //随机数生成器
+        SecureRandom random = new SecureRandom();
+        //声明盐数组变量
+        byte[] salt = new byte[SALT_LENGTH];
+        //将随机数放入盐变量中
+        random.nextBytes(salt);
+
+        //声明消息摘要对象
+        MessageDigest md = null;
+        //创建消息摘要
+        md = MessageDigest.getInstance("MD5");
+        //将盐数据传入消息摘要对象
+        md.update(salt);
+        //将口令的数据传给消息摘要对象
+        md.update(password.getBytes("UTF-8"));
+        //获得消息摘要的字节数组
+        byte[] digest = md.digest();
+
+        //因为要在口令的字节数组中存放盐，所以加上盐的字节长度
+        pwd = new byte[digest.length + SALT_LENGTH];
+        //将盐的字节拷贝到生成的加密口令字节数组的前12个字节，以便在验证口令时取出盐
+        System.arraycopy(salt, 0, pwd, 0, SALT_LENGTH);
+        //将消息摘要拷贝到加密口令字节数组从第13个字节开始的字节
+        System.arraycopy(digest, 0, pwd, SALT_LENGTH, digest.length);
+        //将字节数组格式加密后的口令转化为16进制字符串格式的口令
+        return byteToHexString(pwd);
+    }
+
+    /**
+     * 验证口令是否合法
+     * @param password 输入的密码
+     * @param passwordInDb 数据库获取的加密密码
+     * @return
+     * @throws NoSuchAlgorithmException
+     * @throws UnsupportedEncodingException
+     */
+    public static boolean validPassword(String password, String passwordInDb)
+            throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        //将16进制字符串格式口令转换成字节数组
+        byte[] pwdInDb = hexStringToByte(passwordInDb);
+        //声明盐变量
+        byte[] salt = new byte[SALT_LENGTH];
+        //将盐从数据库中保存的口令字节数组中提取出来
+        System.arraycopy(pwdInDb, 0, salt, 0, SALT_LENGTH);
+        //创建消息摘要对象
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        //将盐数据传入消息摘要对象
+        md.update(salt);
+        //将口令的数据传给消息摘要对象
+        md.update(password.getBytes("UTF-8"));
+        //生成输入口令的消息摘要
+        byte[] digest = md.digest();
+        //声明一个保存数据库中口令消息摘要的变量
+        byte[] digestInDb = new byte[pwdInDb.length - SALT_LENGTH];
+        //取得数据库中口令的消息摘要
+        System.arraycopy(pwdInDb, SALT_LENGTH, digestInDb, 0, digestInDb.length);
+        //比较根据输入口令生成的消息摘要和数据库中消息摘要是否相同
+        if (Arrays.equals(digest, digestInDb)) {
+            //口令正确返回口令匹配消息
+            return true;
+        } else {
+            //口令不正确返回口令不匹配消息
+            return false;
+        }
     }
 
 }
